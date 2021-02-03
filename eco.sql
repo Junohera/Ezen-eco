545bd9883e0b3ce7b3329086666c56cb6e917d54
Juno <juno@Juno-MacBookPro.local>
2021-02-03 22:36:56 +0900

    데이터베이스 초기화 스크립트 추가

--- eco.sql
+++ eco.sql
@@ -1,13 +1,56 @@
+-- drop table
+drop table eco_notice purge;
+drop table eco_qReply purge;
+drop table eco_qna purge;
+drop table eco_music_like purge;
+drop table eco_album_like purge;
+drop table eco_artist_like purge;
+drop table eco_music_reply purge;
+drop table eco_music purge;
+drop table eco_album purge;
+drop table eco_artist purge;
+drop table eco_theme purge;
+drop table eco_genre purge;
+drop table eco_admin purge;
+drop table eco_user purge;
+
+-- drop sequence
+drop sequence eco_user_seq;
+drop sequence eco_admin_seq;
+drop sequence eco_music_seq;
+drop sequence eco_genre_seq;
+drop sequence eco_theme_seq;
+drop sequence eco_music_reply_seq;
+drop sequence eco_album_seq;
+drop sequence eco_artist_seq;
+drop sequence eco_qna_seq;
+drop sequence eco_notice_seq;
+drop sequence eco_qReply_seq;
+
+-- create sequence
+create sequence eco_user_seq start with 1;
+create sequence eco_admin_seq start with 1;
+create sequence eco_music_seq start with 1;
+create sequence eco_genre_seq start with 1;
+create sequence eco_theme_seq start with 1;
+create sequence eco_music_reply_seq start with 1;
+create sequence eco_album_seq start with 1;
+create sequence eco_artist_seq start with 1;
+create sequence eco_qna_seq start with 1;
+create sequence eco_notice_seq start with 1;
+create sequence eco_qReply_seq start with 1;
+
+-- create table
 create table eco_user(
 	useq number(5) primary key,
 	email varchar2(50),
 	pw varchar2(20),
 	name varchar2(30),
 	phone varchar2(13),
-	membership varchar2(1)
+	gender varchar2(1),
+	membership varchar2(1) default 'N',
+	indate date default sysdate
 );
-
-create sequence eco_user_seq start with 1;
 
 create table eco_admin(
 	aseq number(3) primary key,
@@ -15,9 +58,33 @@ create table eco_admin(
 	pw varchar2(20)
 );
 
+create table eco_genre(
+	gseq number(5) primary key,
+	genre varchar2(30)
+);
 
+create table eco_theme(
+	tseq number(5) primary key,
+	theme varchar2(30)
+);
 
-create sequence eco_admin_seq start with 1;
+create table eco_artist (
+	atseq number(5) primary key,
+	name varchar2(30),
+	groupyn varchar2(50),
+	gender varchar2(10),
+	img varchar2(50),
+	description varchar2(1000)
+);
+
+create table eco_album (
+	abseq number(5) primary key,
+	atseq number(5),
+	title varchar2(50),
+	img varchar2(50),
+	content varchar2(1000),
+	pdate date default  sysdate
+);
 
 create table eco_music(
 	mseq number(5) primary key,
@@ -26,24 +93,9 @@ create table eco_music(
 	gseq number(2),
 	title varchar2(30),
 	content varchar2(1000),
-	theme varchar2(3)
+	theme varchar2(3),
+	titleyn varchar2(1)
 );
-
-create sequence eco_music_seq start with 1;
-
-create table eco_genre(
-	gseq number(5) primary key,
-	genre varchar2(30)
-);
-
-create sequence eco_genre_seq start with 1;
-
-create table eco_theme(
-	tseq number(5) primary key,
-	theme varchar2(30)
-);
-
-create sequence eco_theme_seq start with 1;
 
 create table eco_music_reply(
 	rseq number(5) primary key,
@@ -52,8 +104,6 @@ create table eco_music_reply(
 	content varchar2(1000),
 	wdate date default  sysdate
 );
-
-create sequence eco_music_reply_seq start with 1;
 
 create table eco_music_like(
 	mseq number(5),
@@ -70,28 +120,6 @@ create table eco_artist_like(
 	atseq number(5)
 );
 
-create table eco_album (
-	abseq number(5) primary key,
-	atseq number(5),
-	title varchar2(50),
-	img varchar2(50),
-	pdate date default  sysdate,
-	content varchar2(1000)
-);
-
-create sequence eco_album_seq start with 1;
-
-create table eco_artist (
-	atseq number(5) primary key,
-	name varchar2(5),
-	groupyn varchar2(50),
-	gender varchar2(10),
-	img varchar2(50),
-	description varchar2(1000)
-);
-
-create sequence eco_artist_seq start with 1;
-
 create table eco_qna (
 	qseq number(5) primary key,
 	useq number(5),
@@ -99,17 +127,6 @@ create table eco_qna (
 	content varchar2(1000),
 	qna_date date default  sysdate
 );
-
-create sequence eco_qna_seq start with 1;
-
-create table eco_notice (
-	nseq number(5) primary key,
-	title varchar2(50),
-	content varchar2(1000),
-	notice_date date default  sysdate
-);
-
-create sequence eco_notice_seq start with 1;
 
 create table eco_qReply (
 	qrseq number(5) primary key,
@@ -119,4 +136,9 @@ create table eco_qReply (
 	qreply_date date default  sysdate
 );
 
-create sequence eco_qReplyseq start with 1;
+create table eco_notice (
+	nseq number(5) primary key,
+	title varchar2(50),
+	content varchar2(1000),
+	notice_date date default  sysdate
+);
--- insert.sql
+++ insert.sql
@@ -1,2 +1,17 @@
+insert into eco_user(useq, email, pw, name, phone, gender) values(eco_user_seq.nextval, 'selectjuno@gmail.com', '1234', 'juno', '010-2172-7798', 'M');
 insert into eco_admin(aseq, id, pw) values(eco_admin_seq.nextval, 'admin', 'admin');
-select * from eco_admin;
+insert into eco_genre(gseq, genre) values(eco_genre_seq.nextval, 'pop');
+insert into eco_theme(tseq, theme) values(eco_theme_seq.nextval, '시원한');
+insert into eco_theme(tseq, theme) values(eco_theme_seq.nextval, '감성적인');
+insert into eco_artist(atseq, name, groupyn, gender, img, description) values(eco_artist_seq.nextval, 'Ed Sheeran', 'N', 'M', null, '아티스트 소개');
+insert into eco_artist_like(useq, atseq) values(1, 1);
+insert into eco_album(abseq, atseq, title, img, content) values(eco_album_seq.nextval, 1, 'First Album', null, '앨범소개');
+insert into eco_album_like(useq, abseq) values(1, 1);
+insert into eco_music(mseq, abseq, atseq, gseq, title, content, theme, titleyn) values(eco_music_seq.nextval, 1, 1, 1, 'Lego House', '엄청난 첫번째노래 정보', '2', 'Y');
+insert into eco_music(mseq, abseq, atseq, gseq, title, content, theme, titleyn) values(eco_music_seq.nextval, 1, 1, 1, 'One', '엄청난 두번째노래 정보', '1|2', 'N');
+insert into eco_music_like(mseq, useq) values(1, 1);
+insert into eco_music_reply(rseq, mseq, useq, content) values(eco_music_reply_seq.nextval, 1, 1, '개조아여');
+insert into eco_music_reply(rseq, mseq, useq, content) values(eco_music_reply_seq.nextval, 2, 1, '역시... 뒤집어 놓으다.. 음정, 박자.. 와...');
+insert into eco_qna(qseq, useq, title, content) values(eco_qna_seq.nextval, 1, '블루투스 연결은 어떻게 하나요?', '안녕하세요. 해당 웹을 10년간 이용해온 사용자인데요. 혹시 블루투스 연결은 안되나요?');
+insert into eco_qReply(qrseq, qseq, useq, content) values(eco_qReply_seq.nextval, 1, 1, '컴퓨터에 블루투스 장치가 연결되었는지 확인하여주시고, 연결되었음에도 들리지않는다면 해당 장치의 문제이므로 저희사이트와는 무관한 증상입니다.');
+insert into eco_notice(nseq, title, content) values(eco_notice_seq.nextval, 'title', 'content');
