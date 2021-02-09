79dd6bf8527b9b81564a84bea2ec9c087ba58080
Juno <juno@Juno-MacBookPro.local>
2021-02-09 22:14:26 +0900

    내 보관함 좋아요한 곡목록 쿼리 예시

--- sql/select.sql
+++ sql/select.sql
@@ -48,4 +48,17 @@ from artist at
 	join genre g
 		on at.gseq = g.gseq;
 		
-select * from member
+select * from member;
+
+-- 특정유저가 좋아요를 누른 곡목록을 최신별로 조회  
+select * from
+music m, album ab, artist at, music_like ml
+where
+    m.abseq = ab.abseq
+    and m.atseq = at.atseq
+    and ml.mseq = m.mseq
+and ml.useq = 1
+order by cdate desc;
+
+-- 좋아요한 음악들을 유저별 생성시간별로 정렬하여 조회
+select * from music_like order by useq desc, cdate desc;
