59912e0072c09a3c5247fa1beb81712b60009374
juno <selectjuno@gmail.com>
2021-02-09 11:07:15 +0900

    artist.img 100->300, album.img 200 ->500

--- sql/create.sql
+++ sql/create.sql
@@ -117,7 +117,7 @@ create table artist (
 	groupyn varchar2(1) not null, -- Y: 그룹, N: 솔로
 	gender varchar2(1) not null, -- M: 남성, F: 여성, A: 혼성
 	gseq number(5) references genre(gseq),
-	img varchar2(100),
+	img varchar2(300),
 	description varchar2(1000)
 );
 
@@ -125,7 +125,7 @@ create table album (
 	abseq number(5) primary key,
 	atseq number(5) references artist(atseq),
 	title varchar2(50) not null,
-	img varchar2(200),
+	img varchar2(500),
 	content varchar2(3000),
 	pdate date
 );
